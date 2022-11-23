a6d0eaf992b0f327b8dd20dab27dd01299786b42
Shameekh-Naveed <shameekh2002@gmail.com>
2022-11-23 21:45:48 +0500

    Made tables

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-alert("Project Chik Soo")
+
+let opcodes = {
+  Mov: "000000",
+  Add: "000001",
+  Sub: "000010",
+  Mul: "000011",
+  Div: "000100",
+  Inc: "000101",
+  Dec: "000110",
+  And: "000111",
+  Or: "001000",
+  Xor: "001001",
+  Not: "001010",
+  Jz: "001011",
+  Neg: "001100",
+  Shift: "001101",
+};
+
+let D = "0",
+  W = "0";
+let Mod = "00";
+
+let registerA = "000",
+  RegisterB = "000";
